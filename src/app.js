86adb05d7bf911b28232147be859add9a73547cf
skobraf <skobraf@gmail.com>
2018-11-23 17:47:46 +0000

    modify code

--- src/app.js
+++ src/app.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
-
+//React routing goes here
 ReactDOM.render(<p>Expensify app</p>, document.getElementById('app'));
