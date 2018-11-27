bf91c2479dec341bde30e02dbc3cfbee406cef44
skobraf <skobraf@gmail.com>
2018-11-27 19:20:52 +0000

    added routes to app

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom'
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 //React routing goes here
-ReactDOM.render(<p>Expensify app</p>, document.getElementById('app'));
+const ExepnsifyDashboardPage = () => {
+	return (
+		<p>hello</p>
+		);
+}
+
+const AddExpenePage = () => {
+	return (
+		<p>create page</p>
+		)
+}
+const routes = (
+	<BrowserRouter>
+		<div>
+			<Route path="/" component={ExepnsifyDashboardPage} exact={true} />
+			<Route path="/create" component={AddExpenePage} />
+		</div>
+	</BrowserRouter>
+	);
+ReactDOM.render(routes, document.getElementById('app'));
