87f1e6c62a4a2acfb6ff5af265a686f53db44664
skobraf <skobraf@gmail.com>
2018-12-05 21:46:04 +0000

    array distuction

--- src/playground/test.js
+++ src/playground/test.js
@@ -8,3 +8,9 @@ const book = {
 
 const {name: publisherName} = book.publisher;
 console.log(`${publisherName}`);
+
+const adress = ['1299 s junor', 'pheladelphia', 'Pannsaylvania', '19147'];
+
+const [,, yourState] = adress;
+
+console.log(`You are in ${city} ${state} `);
