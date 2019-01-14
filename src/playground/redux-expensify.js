06ca700da4c1343622017a7ab501c45b107896a8
skobraf <skobraf@gmail.com>
2019-01-14 17:23:53 +0000

    added remove Expense

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -13,13 +13,31 @@ const addExpense = ({
 	type: 'ADD_EXPENSE',
 	expense:{
 		id: uuid(),
+		description,
+		note,
+		amount,
+		createdAt
 
 	}
-})
+});
+
+const removeExpense = ({id} = {}) => ({
+	type: 'REMOVE_EXPENSE',
+	id
+
+});
 
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
 	switch(action.type) {
+		case 'ADD_EXPENSE':
+			return [
+				...state,
+				action.expense
+				];
+		case 'REMOVE_EXPENSE':
+			return state.filter(({id}) => id != action.id);
+
 		default:
 			return state;
 	}
@@ -37,7 +55,21 @@ const store = createStore(combineReducers({
 	filters: filtersReducer
 }));
 
-console.log(store.getState());
+store.subscribe(()=>{
+	console.log(store.getState());
+})
+
+const expense1 = store.dispatch(addExpense({
+	description: "Rent",
+	amount: 100
+}));
+
+const expense2 = store.dispatch(addExpense({
+	description: 'Coffee',
+	amount: 200
+}));
+
+store.dispatch(removeExpense({id: expense1.expense.id}));
 
 const demoState = {
 	expenses: [{
