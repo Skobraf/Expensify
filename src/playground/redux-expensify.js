966f1735f87cdf9953bcb6ce831c068c604dbf89
skobraf <skobraf@gmail.com>
2019-01-14 16:18:33 +0000

    ADD expense reducer

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,21 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+//ADD EXPENSE
+
+const addExpense = ({
+	description = '',
+	note = '',
+	amount = 0,
+	createdAt = 0
+		} = {}
+	) => ({
+	type: 'ADD_EXPENSE',
+	expense:{
+		id: uuid(),
+
+	}
+})
 
 const expensesReducerDefaultState = [];
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
